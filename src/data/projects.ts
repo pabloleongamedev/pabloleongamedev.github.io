e574dc116b513ffb4f3f6b492440b6d0232642fa
Pablo Leon <realpleon@gmail.com>
2026-03-02 15:19:35 -0500

    fix links

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -68,7 +68,8 @@ export const PROJECTS = [
         description:
             "Juego del genero Tower Defense, desarrollado en unity con C#. El objetivo del juego es defender tu portería de los ataques enemigos, utilizando la mecanica de movimiento basada en el punto de vista del jugador y utilizando los potenciadores disponibles.",
         link: "https://pleond.itch.io/sumoball-soccer",
-        github: "https://github.com/Phleond/SumoBall-Soccer",
+        github: "https://github.com/pabloleongamedev/SumoBallSoccer",
+        youtube: "https://www.youtube.com/watch?v=9n8mXo2l5jM",
         image: "/projects/sumoBallS.png",
         tags: [TAGS.UNITY, TAGS.CSharp, TAGS.GIT, TAGS.GITHUB],
     },
