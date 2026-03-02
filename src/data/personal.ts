3841c0326aee4eff6546fe7227ac83cf0d532160
Pablo Leon <realpleon@gmail.com>
2026-03-02 15:52:05 -0500

    Update text in profile

--- src/data/personal.ts
+++ src/data/personal.ts
@@ -1,7 +1,7 @@
 export const PERSONAL_INFO = {
     name: "Pablo Leon",
     title: "¿Quién está detrás del código?",
-    description: "Desarrollador de videojuegos enfocado en la creacion de experiencias jugables 3D mediante la implementación de prototipos y sistemas jugables. Disfruto especialmente desarrollar controladores de personaje, sistemas de combate, cámaras, lógica de interacción y estructuras modulares orientadas a la escalabilidad.",
+    description: "Desarrollador de videojuegos enfocado en la creación de experiencias jugables 3D mediante la implementación de prototipos y sistemas jugables. Disfruto especialmente desarrollar controladores de personaje, sistemas de combate, cámaras, lógica de interacción y estructuras modulares orientadas a la escalabilidad.",
     journey: "Mi viaje en el desarrollo de videojuegos comenzó hace varios años, impulsado por mi pasión por los juegos y la tecnología. Desde entonces, he dedicado tiempo a aprender y perfeccionar mis habilidades en programación, especialmente en el ámbito del desarrollo de juegos.",
     skills: ["🚀 Aprendizaje continuo", "💡 Solución de problemas", "🤝 Trabajo en equipo", "  🎯 Orientación al detalle"],
     heroDescription: "Prototype Gameplay Developer especializado en la creación de experiencias jugables en entornos 3D. Disfruto especialmente desarrollar controladores de personaje, sistemas de combate, cámaras, lógica de interacción y estructuras modulares orientadas a la escalabilidad.",
@@ -11,7 +11,7 @@ export const PERSONAL_INFO = {
             href: "https://www.linkedin.com/in/pablo-leon-diaz-game-developer/",
             label: "LinkedIn",
         },
-        { href: "https://github.com/Phleond", label: "GitHub" },
+        { href: "https://github.com/pabloleongamedev", label: "GitHub" },
         { href: "https://x.com/realpleon", label: "Twitter" },
         { href: "mailto:realpleon@gmail.com", label: "Email" },
     ],
