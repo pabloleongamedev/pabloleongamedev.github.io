93e52d0c66ea370aef9a7f5e41aad6c17c311d5b
Pablo Leon <realpleon@gmail.com>
2026-03-02 14:56:49 -0500

    fix text in button

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,12 @@ import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 
 export default defineConfig({
-
   site: 'https://pabloleongamedev.github.io',
-
   output: "static",
-
   integrations: [
     tailwind(),
-
     icon({
       iconDir: "src/icons"
     })
-
   ]
-
 });
